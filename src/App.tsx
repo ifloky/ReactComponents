049d96a3865aac0a6714359e2a78c468cbe61332
iflocky <Petrenko1702@gmail.com>
2023-10-27 16:14:11 +0300

    feat: add loader and save localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ interface SearchPageState {
   searchTerm: string;
   searchResults: [];
   error: string | null;
+  loading: boolean;
 }
 
 class SearchPage extends Component<object, SearchPageState> {
@@ -16,15 +17,36 @@ class SearchPage extends Component<object, SearchPageState> {
       searchTerm: '',
       searchResults: [],
       error: null,
+      loading: false,
     };
   }
 
+  componentDidMount() {
+    const lastSearchRequest = localStorage.getItem('searchRequest');
+    if (lastSearchRequest) {
+      this.handleSearch(lastSearchRequest);
+    }
+  }
+
   handleSearch = async (searchText: string) => {
+    this.setState({ loading: true });
+
     try {
       const searchResults = await getPlanet(searchText);
-      this.setState({ searchResults: searchResults.results, error: null });
+      this.setState({
+        searchResults: searchResults.results,
+        error: null,
+        searchTerm: searchText,
+        loading: false,
+      });
+
+      localStorage.setItem('searchRequest', searchText);
     } catch (error) {
-      this.setState({ searchResults: [], error: 'Error' });
+      this.setState({
+        searchResults: [],
+        error: 'Error',
+        loading: false,
+      });
     }
   };
 
@@ -33,7 +55,11 @@ class SearchPage extends Component<object, SearchPageState> {
       <>
         <h1 className="main-text">What planet are you interested in?</h1>
         <SearchBlock onSearch={this.handleSearch} />
-        <ResultsBlock searchResults={this.state.searchResults} />
+        {this.state.loading ? (
+          <div className="loader">Loading...</div>
+        ) : (
+          <ResultsBlock searchResults={this.state.searchResults} />
+        )}
       </>
     );
   }
